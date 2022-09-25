00c35abb6c21b666a19050ac82132843be9a1aca
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-09-25 18:57:41 +0100

    Set up pages routes for about and products

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
