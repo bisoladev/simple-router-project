1c8dd64dab355f5efa780f54e97e6157ef7f5020
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-09-25 21:24:12 +0100

    Removed dummy nav and footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Error from "./pages/Error";
 function App() {
   return (
     <BrowserRouter>
-      <nav>Our Navbar</nav>
       <Routes>
         <Route path="/" element={<Home />}>
           <Route path="about" element={<About />} />
@@ -15,7 +14,6 @@ function App() {
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
-      <footer>Our Footer</footer>
     </BrowserRouter>
   );
 }
