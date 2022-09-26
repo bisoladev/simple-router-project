2ab58038e68dde0f0b80454024110932944eb133
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-09-26 14:39:32 +0100

    Made changes

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="products" element={<Products />} />
-          <Route path="*" element={<Error />} />
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='products' element={<Products />} />
+          <Route path='*' element={<Error />} />
         </Route>
       </Routes>
     </BrowserRouter>
